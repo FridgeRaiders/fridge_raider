94c4ffea3c39361e7a82208ee224be0a00497e76
carlosbyrne <carlosbyrne@outlook.com>
2026-03-18 15:36:03 +0000

    added ingredient and user model and corresponding basic unit tests

---
+++ src/main/java/com/example/foodproject/model/User.java
@@ -0,0 +1,30 @@
+package com.example.foodproject.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "USERS")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String email;
+
+    private String display_name;
+
+    public User(String email, String display_name) {
+        this.email = email;
+        this.display_name = display_name;
+    }
+
+    public User(String email){
+        this.email = email;
+    }
+}
