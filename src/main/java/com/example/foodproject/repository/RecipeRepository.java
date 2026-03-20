59a7cf5441ed28d712df2d5f07f35a1b62f9235d
Michael Lake <mlake0911@gmail.com>
2026-03-20 12:01:30 +0000

    update repositorys with removed @reposiotry tag and fix sintax in index with new saved button

--- src/main/java/com/example/foodproject/repository/RecipeRepository.java
+++ src/main/java/com/example/foodproject/repository/RecipeRepository.java
@@ -8,7 +8,6 @@ import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
-@Repository
 public interface RecipeRepository extends JpaRepository<Recipe, Long> {
 
     @Query("SELECT r FROM Recipe r WHERE " +
